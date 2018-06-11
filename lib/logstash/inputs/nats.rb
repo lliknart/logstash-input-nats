58fd608410029cda0bdd8300a1ba8415e71b2782
Bitarelle, Benoit <benoit.bitarelle@continental-corporation.com>
2018-06-11 17:52:57 +0200

    some fixes

--- lib/logstash/inputs/nats.rb
+++ lib/logstash/inputs/nats.rb
@@ -5,9 +5,10 @@ require "stud/interval"
 require "nats/io/client"
 require "socket" # for Socket.gethostname
 
+# Add any asciidoc formatted documentation here
 # Generate a repeating message.
 #
-# This plugin is intented only as an example.
+# This plugin is intended only as an example.
 
 class LogStash::Inputs::Nats < LogStash::Inputs::Base
   config_name "nats"
@@ -36,7 +37,7 @@ class LogStash::Inputs::Nats < LogStash::Inputs::Base
   config :pass, :validate => :string, :required => false
 
   # The subject name ti subscribe on
-  config :subject, :validate => :string, :required => true
+  config :subject, :validate => :string, :required => true, :default => "bite"
 
   # The queue group to join if needed
   config :queue_group, :validate => :string, :required => false
@@ -56,10 +57,14 @@ class LogStash::Inputs::Nats < LogStash::Inputs::Base
   # Turns on additional strict format checking
   config :pedantic, :validate => :boolean, :default => false
 
+  # TOREMOVE #bite pour s'en rapeller
+
+  config :interval, :validate => :number, :default => 1
 
   public
   def register
-    @nats_server = @url.nil? ? "nats://#{@user}:#{@pass}@#{@host}:#{@port}" : "#{@url}"
+    #@nats_server = @url.nil? ? "nats://#{@user}:#{@pass}@#{@host}:#{@port}" : "#{@url}"
+    @nats_server = @url.nil? ? "nats://#{@host}:#{@port}" : "#{@url}"
 
     @nats = NATS::IO::Client.new
     @nats.connect(:verbose => @verbose,
@@ -71,68 +76,30 @@ class LogStash::Inputs::Nats < LogStash::Inputs::Base
     @logger.info("Registering Nats", :identity => @identity)
   end # def register
 
-
   def run(queue)
-    received = []
-    @nats.subscribe(:subject => @subject,
-                    :queue_group => @queue_group) do |msg|
-      received << msg
-
-      event = LogStash::Event.new("message" => @message, "host" => @host)
-      decorate(event)
-      queue << event
-      # because the sleep interval can be big, when shutdown happens
-      # we want to be able to abort the sleep
-      # Stud.stoppable_sleep will frequently evaluate the given block
-      # and abort the sleep(@interval) if the return value is true
-      Stud.stoppable_sleep(@interval) { stop? }
-    end # loop
-  end # def run
-
-  def stop
-    # nothing to do in this case so it is not necessary to define stop
-    # examples of common "stop" tasks:
-    #  * close sockets (unblocking blocking reads/accepts)
-    #  * cleanup temporary files
-    #  * terminate spawned threads
-  end
-
-  # private methods ---------------------------
-  private
-
-  def nats_params
-    if @url.nil?
-      connectionParams = {
-        :host => @host,
-        :port => @port
-      }
-
-    else
-      @logger.warn("Parameter 'url' is set, ignoring parameters: 'host' and 'port'")
-      connectionParams = {
-        :servers => @url
-      }
+    @nats.on_error do |e|
+      @logger.info("Error: #{e}")
+      @logger.info(e.backtrace)
     end
 
-    baseParams = {
-            :user => @user.nil? ? nil : @user.value,
-            :pass => @pass.nil? ? nil : @pass.value,
-            :ssl => @ssl,
-            :subject => @subject,
-            :queue_group => @queue_group.nil? ? nil : @queue_group.value,
-            :name => @name.nil? ? nil : @name.value,
-            :private_key_file => @private_key_file.nil? ? nil : @private_key_file.value,
-            :cert_file => @cert_file.nil? ? nil : @cert_file.value,
-            :verbose => @verbose,
-            :pedantic => @pedantic
-    }
+    @nats.on_reconnect do
+      @logger.info("Reconnected to server at #{@nats.connected_server}")
+    end
 
-    return connectionParams.merge(baseParams)
-  end # nats_params
+    @nats.on_disconnect do
+      @logger.info("Disconnected!")
+    end
 
-
-  def new_nats_instance
-    @nats_builder.call
+    @nats.on_close do
+      @logger.info("Connection to NATS closed")
+    end
+    Stud.interval(@interval) do
+      sid = @nats.subscribe(@subject, :queue => @queue_group) { |msg|
+        @logger.info("Received a message: '#{msg}'")
+        event = LogStash::Event.new("message" => msg)
+        decorate(event)
+        queue << event
+      }
+    end
   end
-
-end # class LogStash::Inputs::Nats
+end # class LogStash::Inputs::Example
